6f68fed693281f57f5a5cd74402446d42d45d3f2
Luis Esteban <larinomoriano.esteban@gmail.com>
2025-05-07 00:16:59 -0500

    modified

--- app/src/main/java/com/universidad/ambientalkids/ui/StoreScreen.kt
+++ app/src/main/java/com/universidad/ambientalkids/ui/StoreScreen.kt
@@ -1,19 +1,24 @@
 package com.universidad.finankids.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.universidad.ambientalkids.R
 import com.universidad.ambientalkids.navigation.navigateToScreen
 import com.universidad.ambientalkids.ui.components.BottomMenu
 
