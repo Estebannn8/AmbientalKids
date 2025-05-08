e0ded197684f53e878b0b3bbab6cf268a3b2ff1f
Luis Esteban <larinomoriano.esteban@gmail.com>
2025-05-07 22:43:46 -0500

    modified

--- app/src/main/java/com/universidad/ambientalkids/ui/MainScreen.kt
+++ app/src/main/java/com/universidad/ambientalkids/ui/MainScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.runtime.Composable
@@ -28,6 +29,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.universidad.ambientalkids.R
 import com.universidad.ambientalkids.navigation.AppScreens
+import com.universidad.ambientalkids.ui.components.Background
 import com.universidad.ambientalkids.ui.components.CustomButton
 import com.universidad.ambientalkids.ui.theme.AppTypography
 
@@ -35,22 +37,7 @@ import com.universidad.ambientalkids.ui.theme.AppTypography
 fun MainScreen(navController: NavController) {
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        /*
-        // Imagen de fondo
-        Image(
-            painter = painterResource(id = R.drawable.ic_background_splash),
-            contentDescription = "Fondo de pantalla",
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
-        )
-
-         */
-
-        // Contenido principal sobre la imagen de fondo
+    Background {
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -66,48 +53,32 @@ fun MainScreen(navController: NavController) {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(5.dp)
-                ) {
-                    AppTypography.BalooStroke(
-                        text = "FINANKIDS",
-                        strokeColor = Color(0xFF52154E),
-                        fillColor = Color(0xFF52154E),
-                        fontSize = 50.sp,
-                        letterSpacing = 2.sp,
-                        textAlign = TextAlign.Center,
-                        lineHeight = 22.sp,
-                        strokeWidth = 5.5f
-                    )
-                    /*
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_pesito_logo),
-                        contentDescription = "Logo de Pesito",
-                        modifier = Modifier
-                            .height(50.dp)
-                            .aspectRatio(1.2f)
-                            .offset(y = (-4).dp)
-                    )
-
-                     */
-                }
+                Image(
+                    painter = painterResource(id = R.drawable.ic_logo),
+                    contentDescription = "Ilustración central",
+                    modifier = Modifier.fillMaxSize()
+                        .offset(y = 50.dp),
+                    contentScale = ContentScale.Fit
+                )
             }
 
             // Sección media
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .weight(1f)
+                    .weight(1f) // Ocupa espacio flexible entre header y footer
                     .padding(horizontal = 20.dp, vertical = 15.dp),
                 contentAlignment = Alignment.Center
             ) {
+
                 Image(
                     painter = painterResource(id = R.drawable.ic_pesitos_inicio),
                     contentDescription = "Ilustración central",
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize()
+                        .offset(y = 60.dp),
                     contentScale = ContentScale.Fit
                 )
+
             }
 
             // Botones
@@ -115,23 +86,23 @@ fun MainScreen(navController: NavController) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(screenHeight * 0.29f)
-                    .padding(15.dp),
+                    .offset(y = (-70).dp),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 CustomButton(
                     buttonText = "REGISTRARSE",
-                    gradientLight = Color(0xFF9C749A),
-                    gradientDark = Color(0xFF431441),
-                    baseColor = Color(0xFF53164F),
+                    gradientLight = Color(0xFF4cff91),
+                    gradientDark = Color(0xFF33932a),
+                    baseColor = Color(0xFF5b9848),
                     onClick = { navController.navigate(AppScreens.AuthScreen.createRoute(startInLogin = false)) }
                 )
                 Spacer(modifier = Modifier.height(12.dp))
                 CustomButton(
                     buttonText = "INICIAR SESIÓN",
-                    gradientLight = Color(0xFF9C749A),
-                    gradientDark = Color(0xFF431441),
-                    baseColor = Color(0xFF53164F),
+                    gradientLight = Color(0xFF4cff91),
+                    gradientDark = Color(0xFF33932a),
+                    baseColor = Color(0xFF5b9848),
                     onClick = { navController.navigate(AppScreens.AuthScreen.createRoute(startInLogin = true)) }
                 )
             }
